59da74f8e0eec8ff821959528bed28dd4102bc14
Shubham Verma <subham.varma@reachmobile.com>
2024-07-04 22:16:59 +0530

    added assets and basic sidebar

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="VC Logo"
+          className="mx-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Meeting Meeting
+        </p>
+      </Link>
+      <div className=""></div>
+    </nav>
+  );
+};
+
+export default Navbar;
